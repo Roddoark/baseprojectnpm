2ffcab188c6a88ddfeec5038a983f23dcee1b189
Roddoark <ilyas150691@hotmail.fr>
2017-12-03 18:33:04 +0100

    Learn JS

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
-alert(Math.sin(3.5));
-
-console.log(parseInt("123", 10));
+// alert(Math.sin(3.5));
+var number = 1253;
+console.log(number);
 
 // Math() : permet de gérer certaines fonctions et constantes mathématiques plus avancées (cos, sin, tan, ...)
 // parseInt(): permet de convertir une chaîne de caractères en un nombre entier
@@ -17,12 +17,15 @@ console.log(parseInt("123", 10));
     for ( let variableLet = 0; variableLet < 5; variableLet++ ) {
       // variableLet peut être utilisée ici
     }
-
     // variableLet n'est pas utilisable ici
 // ---------------------------------
 
 // Exemple d'un for...in :
-for (let propriété in objet) {
-  // utiliser des instructions
-  // pour la manipuler la propriété
-}
+// for (let propriété in objet) {
+//   // utiliser des instructions
+//   // pour la manipuler la propriété
+// }
+
+var a = {} // L'objet est vide
+a.toString() // Pourtant il a une méthode toString()
+console.log(a) // Si on regarde le prototype __proto__ on trouve bien la méthode toString()
